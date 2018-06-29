e531a5acfffb7011bdab360f1d88d901cde48bd8
ytallo <ytallolg@gmail.com>
2018-06-29 15:24:29 -0300

    Implementação das migrações

--- migrations/20180629133420_esquema_inicial.js
+++ migrations/20180629133420_esquema_inicial.js
@@ -1,8 +1,11 @@
 
-exports.up = function(knex, Promise) {
+exports.up = knex => knex.schema.createTable("produto", tb => {
+    tb.increments("idproduto")
+    tb.string("nomeproduto").notNullable()
+    tb.string("precoproduto").notNullable()
+    tb.string("descricaoproduto").notNullable()
+  })
   
-};
-
-exports.down = function(knex, Promise) {
-  
-};
+  exports.down = function(knex, Promise) {
+    return knex.schema.dropTable("produto")
+  };
--- migrations/20180629133429_carga_inicial.js
+++ migrations/20180629133429_carga_inicial.js
@@ -1,8 +1,16 @@
+const produtos = [
 
-exports.up = function(knex, Promise) {
-  
-};
+    {idproduto: 1, nomeproduto: "Arroz", precoproduto:"R$10,00", descricaoproduto: "Integral"},
+    {idproduto: 2, nomeproduto: "Macarrão", precoproduto:"R$12,00", descricaoproduto: "Integral"},
+    {idproduto: 3, nomeproduto: "Feijão", precoproduto:"R$14,00", descricaoproduto: "Corda"},
+    {idproduto: 4, nomeproduto: "Bolo", precoproduto:"R$20,00", descricaoproduto: "Confeitado"},
+    {idproduto: 5, nomeproduto: "Carne", precoproduto:"R$30,00", descricaoproduto: "Picanha"},
+    {idproduto: 6, nomeproduto: "Frango", precoproduto:"R$34,00", descricaoproduto: "Peito"},
+    {idproduto: 7, nomeproduto: "PES 2018", precoproduto:"R$100,00", descricaoproduto: "Jogo PS4"},
+    {idproduto: 8, nomeproduto: "FIFA 2018", precoproduto:"R$200,00", descricaoproduto: "Jogo XBox one"}
+]  
 
-exports.down = function(knex, Promise) {
-  
-};
+exports.up = knex => knex("produto").insert(produtos)
+
+exports.down = knex => knex("produto").del()
+  .whereIn("idproduto", produtos.map(e => e.idproduto))
