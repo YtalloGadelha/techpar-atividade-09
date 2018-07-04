64964b7200e5289bec7dc3287d831b184a47bd7b
ytallo <ytallolg@gmail.com>
2018-07-04 17:07:25 -0300

    Comentários no código e passagem do objeto produto pela Intent

--- app-android/app/src/main/java/com/example/ytallo/listaprodutos/MainActivity.kt
+++ app-android/app/src/main/java/com/example/ytallo/listaprodutos/MainActivity.kt
@@ -5,44 +5,55 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
 import com.android.volley.Request
+import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.Volley
+import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.google.gson.reflect.TypeToken
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var listView: ListView
+    lateinit var queue: RequestQueue
+    lateinit var url: String
+    lateinit var jsonArrayRequest: JsonArrayRequest
+    lateinit var jsonList: String
+    lateinit var gson: Gson
+    lateinit var produtoList: List<Produtos>
+    lateinit var adapter: ArrayAdapter<Produtos>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //referêciando a listView a partir do id
-        val listView: ListView = findViewById<ListView>(R.id.list_view)
+        //referêciando a listView a partir do identificador
+        listView = findViewById(R.id.list_view)
 
         // Instanciando a RequestQueue.
-        val queue = Volley.newRequestQueue(this)
+        queue = Volley.newRequestQueue(this)
 
         //Criando a URL
-        val url = "http://192.168.122.1:3000/list"
+        url = "http://192.168.122.1:3000/list"
 
         //Criando a requisição. Verbo GET
-        val jsonArrayRequest = JsonArrayRequest(Request.Method.GET, url, null,
+        jsonArrayRequest = JsonArrayRequest(Request.Method.GET, url, null,
                 Response.Listener { response ->
 
                     //Recebendo string por meio do response
-                    val jsonList = response.toString()
+                    jsonList = response.toString()
 
                     //Instanciando o gson para converter json em objeto
-                    val gson = GsonBuilder().setPrettyPrinting().create()
+                    gson = GsonBuilder().setPrettyPrinting().create()
 
                     //Criando a lista de produtos
-                    var produtoList: List<Produtos> = gson.fromJson(jsonList, object : TypeToken<List<Produtos>>() {}.type)
+                    produtoList = gson.fromJson(jsonList, object : TypeToken<List<Produtos>>() {}.type)
                     println("=== List from JSON ===")
                     produtoList.forEach {println(it)}
 
                     //Criando o adapter necessário ao listView
-                    val adapter = ArrayAdapter<Produtos>(
+                    adapter = ArrayAdapter<Produtos>(
                     this, // Context
                     android.R.layout.simple_list_item_1, // Layout
                     produtoList // List
@@ -51,22 +62,20 @@ class MainActivity : AppCompatActivity() {
                     //Adiconando o adapter ao listView
                     listView.adapter = adapter
 
-                    // Set an item click listener for ListView
+                    // Configurando o click listener da ListView
                     listView.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
 
-//                        Toast.makeText(this, "Produto: " + produtoList[position].nomeproduto + "  Descrição: "
-//                                + produtoList[position].descricaoproduto, Toast.LENGTH_LONG).show()
-
+                        //Criando a Intent para fazer a navegação e passar os dados entre as atividades
                         var intent: Intent = Intent(this, DetalhesActivity::class.java)
+                        intent.putExtra("produto", produtoList[position])
+
                         startActivity(intent)
-
                     }
-
                 },
 
                 Response.ErrorListener { error ->
 
-                    println("Erro!")
+                    println("Erro: ${error}")
                 }
         )
 
